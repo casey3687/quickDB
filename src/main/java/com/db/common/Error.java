801529be9bbb52e730dfefd513c401cae47628f7
casey3687 <1795636964@qq.com>
2025-07-08 15:19:19 +0800

    添加若干Error

--- src/main/java/com/db/common/Error.java
+++ src/main/java/com/db/common/Error.java
@@ -1,6 +1,9 @@
 package com.db.common;
 
 public class Error {
+    // common
+    public static final Exception CacheFullException = new RuntimeException("Cache is full!");
+
     public static final Exception FileExistsException = new RuntimeException("File already exists!");
     public static final Exception FileCannotRWException = new RuntimeException("File cannot read or write!");
     public static final Exception FileNotExistsException = new RuntimeException("File does not exists!");
