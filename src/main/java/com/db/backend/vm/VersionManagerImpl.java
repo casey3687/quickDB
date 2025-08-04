96b22265e5672328e837652f0e1b2be07c9e4789
casey3687 <1795636964@qq.com>
2025-08-04 20:54:29 +0800

    添加VM

---
+++ src/main/java/com/db/backend/vm/VersionManagerImpl.java
@@ -0,0 +1,140 @@
+package com.db.backend.vm;
+
+import com.db.backend.common.AbstractCache;
+import com.db.backend.dm.DataManager;
+import com.db.backend.tm.TransactionManager;
+import com.db.backend.tm.TransactionManagerImpl;
+import com.db.backend.utils.Panic;
+import com.db.common.Error;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class VersionManagerImpl extends AbstractCache<Entry> implements VersionManager {
+
+    TransactionManager tm;
+    DataManager dm;
+    Map<Long, Transaction> activeTransaction;
+    Lock lock;
+
+    public VersionManagerImpl(TransactionManager tm, DataManager dm) {
+        super(0);
+        this.tm = tm;
+        this.dm = dm;
+        this.activeTransaction = new HashMap<>();
+        activeTransaction.put(TransactionManagerImpl.SUPER_XID, Transaction.newTransaction(TransactionManagerImpl.SUPER_XID, 0, null));
+        this.lock = new ReentrantLock();
+    }
+
+
+    /** todo read
+     *
+     * @param xid
+     * @param uid
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public byte[] read(long xid, long uid) throws Exception {
+        return new byte[0];
+    }
+
+    @Override
+    public long insert(long xid, byte[] data) throws Exception {
+        lock.lock();
+        Transaction t = activeTransaction.get(xid);
+        lock.unlock();
+
+        if(t.err != null) {
+            throw t.err;
+        }
+
+        byte[] raw = Entry.wrapEntryRaw(xid, data);
+        return dm.insert(xid, raw);
+    }
+
+    /**
+     *  todo delete
+     * @param xid
+     * @param uid
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean delete(long xid, long uid) throws Exception {
+        return true;
+    }
+
+    @Override
+    public long begin(int level) {
+        lock.lock();
+        try {
+            long xid = tm.begin();
+            Transaction t = Transaction.newTransaction(xid, level, activeTransaction);
+            activeTransaction.put(xid, t);
+            return xid;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void commit(long xid) throws Exception {
+        lock.lock();
+        Transaction t = activeTransaction.get(xid);
+        lock.unlock();
+
+        try {
+            if(t.err != null) {
+                throw t.err;
+            }
+        } catch(NullPointerException n) {
+            System.out.println(xid);
+            System.out.println(activeTransaction.keySet());
+            Panic.panic(n);
+        }
+
+        lock.lock();
+        activeTransaction.remove(xid);
+        lock.unlock();
+
+        tm.commit(xid);
+    }
+
+    @Override
+    public void abort(long xid) {
+        internAbort(xid, false);
+    }
+
+    private void internAbort(long xid, boolean autoAborted) {
+        lock.lock();
+        Transaction t = activeTransaction.get(xid);
+        if(!autoAborted) {
+            activeTransaction.remove(xid);
+        }
+        lock.unlock();
+
+        if(t.autoAborted) return;
+        tm.abort(xid);
+    }
+
+    public void releaseEntry(Entry entry) {
+        super.release(entry.getUid());
+    }
+
+    @Override
+    protected Entry getForCache(long uid) throws Exception {
+        Entry entry = Entry.loadEntry(this, uid);
+        if(entry == null) {
+            throw Error.NullEntryException;
+        }
+        return entry;
+    }
+
+    @Override
+    protected void releaseForCache(Entry entry) {
+        entry.remove();
+    }
+    
+}
